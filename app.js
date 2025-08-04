7a44ec8bc40887344e56a6955fd08194bfe564f6
meeran11 <meeran.official1255@gmail.com>
2025-08-04 18:31:32 +0500

    Modifying package.json

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(cookieParser());
 app.use('/', indexRoutes);
 app.use('/user', userRoutes);
 
-app.listen(3000, () => {
-    console.log('Server is running on port 4000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
