39c8575447c96bdfdbff00b57b1f53d75ad389fd
meeran11 <meeran.official1255@gmail.com>
2025-08-05 18:53:59 +0500

    Trying

--- app.js
+++ app.js
@@ -1,37 +1,13 @@
-const dotenv = require('dotenv').config();
+// app.js
+
 const express = require('express');
-const userRoutes = require('./routes/user.routes');
-const indexRoutes = require('./routes/index.routes');
-const cookieParser = require('cookie-parser');
-const connectDB = require('./config/db');
-connectDB();
-
 const app = express();
-const { body , validationResult } = require('express-validator')
-
-app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(cookieParser());
-
-const path = require('path');
-app.use(express.static(path.join(__dirname, 'public')));
-// const path = require('path');
-app.get('/favicon.ico', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'favicon.ico'));
-});
-
-
-app.get("/", (req, res) => {
-    res.send("Hello!")
-    // res.redirect("/user/login");
-  });
-//app.use('/', indexRoutes);
-app.use('/user', userRoutes);
 
 const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send("🚀 Server is live!"));
+app.get('/favicon.ico', (req, res) => res.status(204)); // no content
+
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`✅ Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
-
-
