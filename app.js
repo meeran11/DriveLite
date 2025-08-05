59cd9f459863a9b2a6e0a7c585e48d5efddeba91
meeran11 <meeran.official1255@gmail.com>
2025-08-05 18:10:59 +0500

    502

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use('/', indexRoutes);
 app.use('/user', userRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`✅ Server running on port ${PORT}`);
 });
+
