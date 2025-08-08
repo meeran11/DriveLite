9452c6bbdb21ed7eccf01fef45a1ec8d70c0161f
meeran11 <meeran.official1255@gmail.com>
2025-08-08 20:37:07 +0500

    DOne!

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use('/', indexRoutes);
 app.use('/user', userRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
