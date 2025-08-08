c25be8f4b01666e91850d532cb70c962225c2d45
meeran11 <meeran.official1255@gmail.com>
2025-08-08 20:25:06 +0500

    Fix

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req, res) => {
     res.render('index')
-    // res.redirect("/user/login");
   });
 app.use('/', indexRoutes);
 app.use('/user', userRoutes);
